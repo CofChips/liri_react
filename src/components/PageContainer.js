0362f5c7f2074e1940810e633e1968d1357a1d7a
CofChips <qiwei.mod@gmail.com>
2021-01-31 11:16:29 -0800

    Basic page display, basic card setup

---
+++ src/components/PageContainer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Card from "./Card";
+
+function PageContainer(){
+
+
+        return (
+            <Card heading="Test">
+                <p>Body</p>
+            </Card>
+        )
+    
+}
+
+export default PageContainer;
